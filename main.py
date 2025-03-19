6261ed77349a4a421ca755838ec1935eafad612f
jonatha silva <jonatha.sviegas@hotmail.com>
2025-03-18 22:09:58 -0300

    Criptografia e descriptografia Comentado

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ from cryptography.hazmat.primitives import serialization, hashes
 
 def fill_pings(path):
     """
-        Preenche a matriz 'path' com valores aleatórios e simétricos com a diagonal zerada.
+        Preenche a matriz 'path' com valores aleatórios e simétricos com a diagonal zerada
     """
-    n=10    # Tamanho da matriz.
-    path = np.random.randint(1,101, size=(n,n))  # Números entre esse intervalo.
+    n=10    # Tamanho da matriz
+    path = np.random.randint(1,101, size=(n,n))  # Números entre esse intervalo
     for i in range(n):
         for j in range(i+1, n):
-            path[j, i] = path[i, j]  # Torna a matriz simétrica.
+            path[j, i] = path[i, j]  # Torna a matriz simétrica
     np.fill_diagonal(path,0)
     return path    
 
 def find_shortest_way(path):
     """
-        Encontra o menor caminho. Atualizando os pings com uma variação de 30% a cada vez que avança um passo.
+        Encontra o menor caminho. Atualizando os pings com uma variação de 30% a cada vez que avança um passo
     """
     current_node = 0
     visited_nodes = [False]*len(path)
@@ -45,15 +45,16 @@ def find_shortest_way(path):
     return total_cost, way
 
 def generate_keys():
+    #Gera a chave privada
     private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048
+        public_exponent=65537, #Valor padrão para RSA
+        key_size=2048 #Tamanho da chave
     )
-    public_key = private_key.public_key()
+    public_key = private_key.public_key() #Extrai a chave pública
     return public_key, private_key
 
 def encrypt_message(message, public_key):
-    message_encoded = message.encode()
+    message_encoded = message.encode() #Converte string para bytes
     crypted_message = public_key.encrypt(
         message_encoded,
         padding.OAEP(
@@ -65,7 +66,8 @@ def encrypt_message(message, public_key):
     return(crypted_message)
 
 def decrypt_message(crypted_message, private_key):
-    decrypted_message = private_key.decrypt(
+    #Usa a chave privada para descriptografar
+    decrypted_message = private_key.decrypt( 
     crypted_message,
     padding.OAEP(
         mgf=padding.MGF1(algorithm=hashes.SHA256()),
