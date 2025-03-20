c28fa96599c486dc6fdf2558dd1e66b1c4b5e471
jonatha silva <jonatha.sviegas@hotmail.com>
2025-03-20 18:29:47 -0300

    antes de mudar o principal com chain

--- main.py
+++ main.py
@@ -1,7 +1,64 @@
 import numpy as np
 import random
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives import hashes
+import hashlib
+import json
+from time import time
+
+
+class Block:
+    def __init__(self , index , timestamp , data , previous_hash , nonce=0):
+        self.index=index # Posição do bloco na cadeia
+        self.timestamp=timestamp  # Momento de criação do bloco
+        self.data=data # Dados
+        self.previoushash=previous_hash # Hash do bloco anterior
+        self.nonce=nonce # Número para prova de trabalho (Proof-of-Work)
+        self.hash=self.compute_hash() # Hash atual do bloco
+
+    def compute_hash(self):
+        """
+        Calcula o hash do bloco convertendo seus atributos para uma string JSON e aplicando SHA-256
+        """
+        block_string = json.dumps(self.__dict__ , sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self , dificulty=2):
+        self.chain=[]
+        self.dificulty=dificulty
+        self.create_genesis_block()
+    
+    def create_genesis_block(self):
+         """
+        Cria o bloco gênesis (primeiro bloco da cadeia).
+        """
+         genesis_block = Block(0 , time() , "Bloco Gênesis" , "0" )
+         self.chain.append(genesis_block)
+
+    def get_last_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self , block):
+        """
+        Executa o algoritmo de prova de trabalho.
+        """
+        block.nonce = 0
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0'*self.dificulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+        return computed_hash
+    
+    def add_block(self , block):
+        """
+        Adiciona um novo bloco à cadeia depois de executar a prova de trabalho.
+        """
+        block.previous_hash = self.get_last_block().hash
+        block.hash = self.proof_of_work(block)
+        self.chain.append(block)
+
+
 
 
 def fill_pings(path):
@@ -14,17 +71,19 @@ def fill_pings(path):
         for j in range(i+1, n):
             path[j, i] = path[i, j]  # Torna a matriz simétrica
     np.fill_diagonal(path,0)
-    return path    
+    return path , n
 
-def find_shortest_way(path):
+def find_shortest_way(path , message):
     """
         Encontra o menor caminho. Atualizando os pings com uma variação de 30% a cada vez que avança um passo
     """
     current_node = 0
     visited_nodes = [False]*len(path)
+    crypted_message = [None]*len(path)
     visited_nodes[current_node] = True
     way = [current_node]
     total_cost = 0
+    crypted_message[current_node]=message
     for _ in range(len(path)-1):
         lower = float('inf')
         next_row = -1
@@ -33,6 +92,8 @@ def find_shortest_way(path):
                 lower = cost
                 next_row=index
         total_cost+=lower
+        crypted_message[next_row]=crypted_message[current_node]
+        crypted_message[current_node]=None
         current_node=next_row
         visited_nodes[current_node] = True
         way.append(current_node)
@@ -79,13 +140,19 @@ def decrypt_message(crypted_message, private_key):
 
 
 
-# path = []
-# path = fill_pings(path)
-# print(path)
-# final_cost, final_way = find_shortest_way(path)
-# print(f"O custo final foi de: {final_cost} e este foi o caminho percorrido: {final_way}")
 
-
+path = []
+message = input("Digite a mensagem: ")
+public_key , private_key = generate_keys()
+message = encrypt_message(message , public_key)
+path, n = fill_pings(path)
+final_cost, final_way = find_shortest_way(path , message)
+print(f"O custo final foi de: {final_cost} e este foi o caminho percorrido: {final_way}")
+if final_way[-1] == n-1:
+    message = decrypt_message(message, private_key)
+    print(message.decode())
+else:
+    print("Error")
 
 
 
